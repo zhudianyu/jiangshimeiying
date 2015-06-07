7409bb83cd4486263bb5f4fd0b5e2a550ce90112
zhudianyu <1462415060@qq.com>
2015-06-07 20:53:42 +0800

    完成场景加载时进度条的显示

--- Assets/_Scripts/StartButton.cs
+++ Assets/_Scripts/StartButton.cs
@@ -4,10 +4,14 @@ using System.Collections;
 public class StartButton : MonoBehaviour {
 
 	// Use this for initialization
-  
+    public GameObject loadObject;
+    public GameObject canvas;
     public  void StartGame()
     {
-        Application.LoadLevel("Demo2");
+        loadObject.SendMessage("LoadNextLevel");
+        //因为和ugui冲突 所以把canvas禁用掉
+        canvas.SetActive(false);
+     //   Application.LoadLevel("Demo2");
     }
 
 }
