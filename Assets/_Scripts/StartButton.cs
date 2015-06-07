679bcf353108435947140abbfb3437a4ccf9d855
zhudianyu <1462415060@qq.com>
2015-06-07 20:21:17 +0800

    完成登录UI

---
+++ Assets/_Scripts/StartButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour {
+
+	// Use this for initialization
+  
+    public  void StartGame()
+    {
+        Application.LoadLevel("Demo2");
+    }
+
+}
