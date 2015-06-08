5cab14efbe277d6f995e81d9002dca69a903c504
zhudianyu <1462415060@qq.com>
2015-06-08 19:41:14 +0800

    通过球形触发器让僵尸追击玩家

---
+++ Assets/_Scripts/FindPlayerCtronl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+//通过球形触发器判断是否追击玩家
+public class FindPlayerCtronl : MonoBehaviour {
+
+  public  EnemyCtronl enemCtrol;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnTriggerEnter(Collider player)
+    {
+        if(player.tag.Equals(GlobalCtrol.PLAYER))
+        {
+            enemCtrol.isFindePlayer = true;
+        }
+        
+    }
+    void OnTriggerExit(Collider player)
+    {
+        if (player.tag.Equals(GlobalCtrol.PLAYER))
+        {
+            enemCtrol.isFindePlayer = false;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
